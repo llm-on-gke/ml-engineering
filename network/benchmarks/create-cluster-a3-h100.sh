18f772a346b19f47ded3518272256690493b62e3
Rick(Rugui) Chen <rickruguichen@google.com>
2024-07-18 16:03:15 -0400

    a3 all-reduce

--- network/benchmarks/create-cluster-a3-h100.sh
+++ network/benchmarks/create-cluster-a3-h100.sh
@@ -1,7 +1,7 @@
 # for L4 and spot node-pools
 export PROJECT_ID=<your-project-id>
 
-export REGION=us-central1
+export REGION=europe-west4
 export ZONE_1=${REGION}-a # You may want to change the zone letter based on the region you selected above
 export ZONE_2=${REGION}-b # You may want to change the zone letter based on the region you selected above
 export CLUSTER_NAME=two-sigma-cluster
@@ -10,21 +10,15 @@ gcloud config set project "$PROJECT_ID"
 gcloud config set compute/region "$REGION"
 gcloud config set compute/zone "$ZONE_1"
 
-gcloud container clusters create $CLUSTER_NAME --location ${REGION} \
-  --workload-pool ${PROJECT_ID}.svc.id.goog \
-  --enable-image-streaming --enable-shielded-nodes \
-  --shielded-secure-boot --shielded-integrity-monitoring \
-  --enable-ip-alias \
-  --node-locations=$REGION-b \
-  --workload-pool=${PROJECT_ID}.svc.id.goog \
-  --addons GcsFuseCsiDriver   \
-  --no-enable-master-authorized-networks \
-  --machine-type n2d-standard-4 \
-  --num-nodes 1 --min-nodes 1 --max-nodes 3 \
-  --ephemeral-storage-local-ssd=count=2 \
-  --scopes="gke-default,storage-rw"
-
-gcloud container node-pools create a2-pool   --accelerator type=nvidia-tesla-a100,count=8,gpu-driver-version=latest   --machine-type a2-highgpu-8g   --region us-central1 --cluster two-sigma-cluster     --node-locations us-central1-b   --enable-autoscaling    --min-nodes 0    --num-nodes 0 --max-nodes 4   --ephemeral-storage-local-ssd=count=0 --spot
-
-
-gcloud container node-pools create a2-gvnic-pool --accelerator type=nvidia-tesla-a100,count=8,gpu-driver-version=latest  --machine-type a2-highgpu-8g   --region us-central1 --cluster two-sigma-cluster  --node-locations us-central1-b   --enable-autoscaling    --min-nodes 0    --num-nodes 0 --max-nodes 4 --ephemeral-storage-local-ssd=count=0 --spot --enable-gvnic
+## default cluster
+export CLUSTER_ARGUMENTS=" \
+ --network=natt-a3-base-sysnet \
+ --subnetwork=gke-subnet \
+ --scopes=storage-full,gke-default \
+ --enable-ip-alias \
+ --enable-private-nodes \
+ --master-ipv4-cidr 172.16.0.32/28 \
+ --cluster-ipv4-cidr=10.224.0.0/12 \
+ --no-enable-master-authorized-networks \
+ --release-channel regular \
+"
